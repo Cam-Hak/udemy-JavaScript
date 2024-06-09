742cd7f7737bb8b256a70d2f6898733af0324937
Cameron <camhakenson@gmail.com>
2024-06-09 10:49:37 -0400

    finished 03

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const printForecast = function (arr) {
+  let forecast = '... ';
+  for (let i = 1; i <= arr.length; i++) {
+    forecast += `${arr[i - 1]} C in ${i} days ... `;
+  }
+  console.log(forecast);
+};
+
+printForecast([17, 21, 23]);
+printForecast([12, 5, -5, 0, 4]);
