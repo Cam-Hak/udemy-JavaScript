439770ca2800bf78f4608306a46936d54cb91cf8
Cameron Hakenson <camhakenson@gmail.com>
2024-06-14 16:11:20 -0400

    more

--- Notes/09-DS-MO-Strings.md
+++ Notes/09-DS-MO-Strings.md
@@ -80,6 +80,7 @@ console.log(...arr);
 [x, y, ...others] = arr;
 // prints 1 2 3 4
 ```
+---
 ## _Short Circuiting_
 - Allows you to set a default value to certain parts of your code
 - When using OR, the first value is returned if its a truthy value
@@ -103,3 +104,46 @@ const name = null;
 name ??= 'Cameron';
 // sets name to Cameron if it wasn't previously set
 ```
+---
+## _For-of Loop_
+- Considered to be an enhanced for-loop in java
+- You can use the .entries() function to get access to the arrays indices as well as the elements
+```js
+const arr = [1, 2, 3, 4];
+// standard for-of loop
+for (const num of arr) {
+    console.log(num); // 1, 2, 3, 4
+}
+
+// enumerated for-of loop
+for (const [i, num] of arr.entries()) {
+    console.log(1, num); // 0 1, 1 2, 2 3, 3 4
+}
+```
+---
+## _Optional Chaining_
+- An efficient way of getting data from an object only if it exists
+- Use the ?. syntax
+- Returns undefined if the value does not pass
+```js
+const obj = {
+    daysOpen = {
+        Monday: {
+            open = true,
+            hours = '5',
+        },
+        Friday: {
+            open = true,
+            hours = '8',
+        },
+    },
+};
+daysCheck = ['Monday', 'Wednesday', 'Friday'];
+
+for (const day of daysCheck) {
+    // prints 5, Not open on that day, 8
+    console.log(obj.daysOpen[day]?.hours ?? 'Not open on that day');
+}
+// will only print the hours if that particular day exists in the object
+```
+---
--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -72,44 +72,58 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 
 GOOD LUCK 😀
 */
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// const [players1, players2] = game.players;
+// const [gk, ...fieldPlayers] = game.players1;
+// const allPlayers = [...players1, ...players2];
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Peristic'];
+
+// const arr = [1, 2, 3, 4];
+// for (const number of arr) {
+//   console.log(number);
+// }
+
+// for (const [i, num] of arr.entries()) {
+//   console.log(i, num);
+// }
 
